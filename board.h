f403f4718411f7b86bb8bc116b74048895a4b39c
kollariswain <163773115+kollariswain@users.noreply.github.com>
2024-03-24 15:32:59 +0300

    Create board.h

---
+++ board.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class basketci {
+public:
+    basketci(int sayi2, int asist2, int rebound2, int block2, int steal2);
+    void yazdir();
+private:
+    int sayi;
+    int asist;
+    int rebound;
+    int block;
+    int steal;
+};
